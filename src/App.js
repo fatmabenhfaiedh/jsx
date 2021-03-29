401dab53ef4a8a961f616c88b648367b666e5a67
fatmabenhfaiedh <79193083+fatmabenhfaiedh@users.noreply.github.com>
2021-03-29 12:51:06 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import image from "./WestAfrican.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div style={{border:"solid 1 black",maxWidth:"100"}}>
+
+    <h1 className="title red">Mamady Keita The Djembefola</h1>
+    <br/>
+    <img src={"./dsc8090-1366w-1.jpg"} alt= ""/>
+    <br/>
+    <img src={image} alt= "NO"/>
+    <br/>
+    <video className= "video" style= {{width:"640",height:"760"}} controls autoPlay>
+      <source src="./djembefola.mp4" type="video/mp4"/>
+    </video>
+
+  </div>
+
+
   );
 }
 
